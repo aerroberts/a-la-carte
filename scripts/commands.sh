1817e09f1cf16710eb5956f8855e6d66db63d0e5
Eric <eric@aerrobert.com>
2025-06-11 00:02:09 -0700

    remove old scripts

--- scripts/commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Opens the scripts folder in your file system to show where the commands are stored
-
-# Get the scripts directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Open the scripts directory in finder
-open "$SCRIPT_DIR"
