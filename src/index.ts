092741cf7dabbf2e9222d22f47266b9070f9b38f
Eric <eric@aerrobert.com>
2025-06-12 22:36:05 -0700

    with code prefix for scoping

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ function main() {
 
     program.name("a-la-carte").description("A hungry developer's toolbox").version("0.0.1");
 
-    // Register commands
-    new ShoveCommand().register(program);
+    // Top-level "code" group command for commands related to coding
+    const code = program.command("code").description("Code related utilities");
+
+    // Register subcommands under "code"
+    new ShoveCommand().register(code);
 
     program.parse();
 }
